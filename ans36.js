270c7168f6661ace746e4939bf0f79578f3fe6b5
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-12 09:57:33 +0500

    Initial commit

---
+++ ans36.js
@@ -0,0 +1,5 @@
+//T-Shirt: Create a function for customizing t-shirts.
+function manufactingT_shirt(Brand, Sizes, Print) {
+    console.log("Manufacturing a ".concat(Brand, " Tshirt of ").concat(Sizes, " size with the message \"").concat(Print, " print on it."));
+}
+manufactingT_shirt("Levi's", 34, "Can't HEAR you I'm C O D I N G");
